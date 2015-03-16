72f57ed246c7db4b9253f3fabe8ea13f2ab0eca3
lin <lin@localhost.localdomain>
2015-03-16 10:36:02 -0700

    2

---
+++ b.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: b.cpp
+	> Author: tan
+	> Mail:874690668@qq.com 
+	> Created Time: Mon 16 Mar 2015 10:35:02 AM PDT
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+
+cout << b << endl;
+return 0;
+}
