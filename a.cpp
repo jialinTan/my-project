0239619cd0d4ff7abfb828a86383705332a80719
lin <lin@localhost.localdomain>
2015-03-16 09:30:20 -0700

    1

---
+++ a.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: a.cpp
+	> Author: tan
+	> Mail:874690668@qq.com 
+	> Created Time: Mon 16 Mar 2015 09:28:43 AM PDT
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+cout << hello << endl;
+return 0;
+
+}
